d69403bbc176da06f57d01b8de602aa9306852dd
Jay Bennett <jay@honeybadgergames.ca>
2016-01-04 09:10:20 +0000

    baseline sql cleanup

--- sql/baseline.sql
+++ sql/baseline.sql
@@ -1,5 +1,22 @@
 
-DROP TABLE "characters";
+CREATE TABLE "games" (
+	"game_id" SERIAL PRIMARY KEY,
+	"map_name" TEXT NOT NULL,
+	"game_mode" TEXT NOT NULL,
+	"minimum_level" INTEGER DEFAULT 0,
+	"player_count" INTEGER DEFAULT 0,
+	"maximum_players" INTEGER DEFAULT 16
+);
+
+CREATE TABLE "account_data" (
+	"user_id" SERIAL PRIMARY KEY,
+	"username" TEXT NOT NULL,
+	"password" BYTEA NOT NULL,
+	"salt" BYTEA NOT NULL,
+	"algorithm" TEXT NOT NULL,
+	"createdon" TIMESTAMP NOT NULL,
+	"lastlogin" TIMESTAMP NOT NULL
+);
 
 CREATE TABLE "characters" (
 	"id" SERIAL PRIMARY KEY,
@@ -9,45 +26,14 @@ CREATE TABLE "characters" (
 	"last_game_id" INTEGER DEFAULT 0
 );
 
-DROP TABLE "game_servers";
-DROP TABLE "games";
 
-
-CREATE TABLE IF NOT EXISTS "games" (
-	"game_id" SERIAL PRIMARY KEY,
-	"map_name" TEXT NOT NULL,
-	"game_mode" TEXT NOT NULL
-	"minimum_level" INTEGER DEFAULT 0
-	"player_count" INTEGER DEFAULT 0
-	"maximum_players" INTEGER DEFAULT 16
-);
-
-CREATE TABLE IF NOT EXISTS "game_servers" (
-	"game_id" SERIAL PRIMARY KEY references games(game_id),
-	"machine_id" SERIAL references machines(machine_id),
-	"port" INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS "account_data" (
-	"user_id" SERIAL PRIMARY KEY,
-	"username" TEXT NOT NULL,
-	"password" BYTEA NOT NULL,
-	"salt" BYTEA NOT NULL,
-	"algorithm" TEXT NOT NULL,
-	"createdon" TIMESTAMP NOT NULL,
-	"lastlogin" TIMESTAMP NOT NULL
-);
-DELETE FROM machines;
-DROP TABLE machines_metadata;
-DROP TABLE machines;
-
-CREATE TABLE IF NOT EXISTS "machines" (
+CREATE TABLE "machines" (
 	"machine_id" SERIAL PRIMARY KEY,
 	"remote_address" TEXT,
 	"service_listen_port" INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS "machines_metadata" (
+CREATE TABLE "machines_metadata" (
 	"machine_id" INTEGER PRIMARY KEY references machines(machine_id) ON DELETE CASCADE,
 	"most_recent_key" TEXT,
 	"last_heartbeat" TIMESTAMP,
@@ -56,7 +42,13 @@ CREATE TABLE IF NOT EXISTS "machines_metadata" (
 	"player_occupancy_pct" REAL
 );
 
-CREATE OR REPLACE FUNCTION get_available_machine()
+CREATE TABLE "game_servers" (
+	"game_id" SERIAL PRIMARY KEY references games(game_id),
+	"machine_id" SERIAL references machines(machine_id),
+	"port" INTEGER
+);
+
+CREATE FUNCTION get_available_machine()
 	RETURNS TABLE (
 		"remote_address" TEXT,
 		"service_listen_port" INTEGER,
